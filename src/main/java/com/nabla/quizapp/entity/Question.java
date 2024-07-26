4490f8ee0aca260103ec20d6500dc3d6ebe509c9
wahiiid <abdulwahidmulla9@gmail.com>
2024-07-26 20:32:20 +0530

    crated entity for Question

---
+++ src/main/java/com/nabla/quizapp/entity/Question.java
@@ -0,0 +1,28 @@
+package com.nabla.quizapp.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String questionTitle;
+    private String option1;
+    private String option2;
+    private String option3;
+    private String option4;
+    private String rightAnswer;
+    private String difficultylevel;
+    private String category;
+}
