1f4ec143033336152de07c3ff2cc1db9a1bb07a5
wahiiid <abdulwahidmulla9@gmail.com>
2024-07-26 16:37:00 +0530

    Initial commit

---
+++ src/main/java/com/nabla/quizapp/QuestionController.java
@@ -0,0 +1,15 @@
+package com.nabla.quizapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+
+    @GetMapping("allQuestions")
+    public String getAllQuestion() {
+        return "Hi these are all your questions....";
+    }
+}
