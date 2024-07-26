18dfdcef75144a793d8ca79549df15340a47ef55
wahiiid <abdulwahidmulla9@gmail.com>
2024-07-26 20:34:48 +0530

    created service layer for Quiz

---
+++ src/main/java/com/nabla/quizapp/service/QuizService.java
@@ -0,0 +1,62 @@
+package com.nabla.quizapp.service;
+
+import com.nabla.quizapp.dao.QuestionDao;
+import com.nabla.quizapp.dao.QuizDao;
+import com.nabla.quizapp.entity.Question;
+import com.nabla.quizapp.entity.QuestionWrapper;
+import com.nabla.quizapp.entity.Quiz;
+import com.nabla.quizapp.entity.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class QuizService {
+
+    @Autowired
+    QuizDao quizDao;
+
+    @Autowired
+    QuestionDao questionDao;
+
+    public ResponseEntity<String> createQuiz(String category, int numQ, String title) {
+
+        List<Question> questions = questionDao.findRandomQuestionsByCategory(category, numQ);
+
+        Quiz quiz = new Quiz();
+        quiz.setTitle(title);
+        quiz.setQuestions(questions);
+        quizDao.save(quiz);
+        return new ResponseEntity<>("Success", HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
+        Optional<Quiz> quiz = quizDao.findById(id);
+        List<Question> questionFromDB = quiz.get().getQuestions();
+        List<QuestionWrapper> questionsForUser = new ArrayList<>();
+        for (Question q : questionFromDB) {
+            QuestionWrapper qw = new QuestionWrapper(q.getId(), q.getQuestionTitle(), q.getOption1(), q.getOption2(), q.getOption3(), q.getOption4());
+            questionsForUser.add(qw);
+        }
+
+        return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Integer> calculateResult(Integer id, List<Response> responses) {
+        Quiz quiz = quizDao.findById(id).get();
+        List<Question> questions = quiz.getQuestions();
+        int right = 0;
+        int i = 0;
+        for (Response response : responses) {
+            if (response.getResponse().equals(questions.get(i).getRightAnswer()))
+                right++;
+            i++;
+        }
+        return new ResponseEntity<>(right, HttpStatus.OK);
+    }
+}
